558cd9e9f272f1014f2aaa80e79cfb17778c24ed
Shiuan <berserker850330@gmail.com>
2023-04-10 10:53:29 +0800

    localStorage setting

--- app.js
+++ app.js
@@ -50,13 +50,118 @@ navbutton.onclick = () => {
   hiddenLink.classList.toggle("hidden-link-move");
 };
 
-// ---------------
-let like = document.querySelector(".like");
-let add = document.querySelector(".heart");
-let delet = document.querySelector(".redheart");
-add.addEventListener("click", (e) => {
-  // let creatHeart = document.createElement("div");
-  // creatHeart.classList.add("redheart");
-  // like.appendChild(creatHeart);
-  console.log(e.target);
+// ------localStorage---------
+const heart1 = document.querySelector("#heart1");
+
+let getmode = localStorage.getItem("mode");
+
+if (getmode && getmode === "redheart") {
+  heart1.classList.add("redheart");
+}
+heart1.addEventListener("click", () => {
+  heart1.classList.toggle("redheart");
+  if(!heart1.classList.contains("redheart")){
+    return localStorage.setItem("mode","heart")
+  }
+  localStorage.setItem("mode","redheart")
 });
+
+
+const heart2 = document.querySelector("#heart2");
+
+let getmode2 = localStorage.getItem("mode2");
+
+if (getmode2 === "redheart") {
+  heart2.classList.add("redheart");
+}
+heart2.addEventListener("click", () => {
+  heart2.classList.toggle("redheart");
+  if(!heart2.classList.contains("redheart")){
+    return localStorage.setItem("mode2","heart")
+  }
+  localStorage.setItem("mode2","redheart")
+});
+
+
+const heart3 = document.querySelector("#heart3");
+
+let getmode3 = localStorage.getItem("mode3");
+
+if (getmode3 === "redheart") {
+  heart3.classList.add("redheart");
+}
+heart3.addEventListener("click", () => {
+  heart3.classList.toggle("redheart");
+  if(!heart3.classList.contains("redheart")){
+    return localStorage.setItem("mode3","heart")
+  }
+  localStorage.setItem("mode3","redheart")
+});
+
+
+
+const heart4 = document.querySelector("#heart4");
+
+let getmode4 = localStorage.getItem("mode4");
+
+if (getmode4 === "redheart") {
+  heart4.classList.add("redheart");
+}
+heart4.addEventListener("click", () => {
+  heart4.classList.toggle("redheart");
+  if(!heart4.classList.contains("redheart")){
+    return localStorage.setItem("mode4","heart")
+  }
+  localStorage.setItem("mode4","redheart")
+});
+
+
+
+const heart5 = document.querySelector("#heart5");
+
+let getmode5 = localStorage.getItem("mode5");
+
+if (getmode5 === "redheart") {
+  heart5.classList.add("redheart");
+}
+heart5.addEventListener("click", () => {
+  heart5.classList.toggle("redheart");
+  if(!heart5.classList.contains("redheart")){
+    return localStorage.setItem("mode5","heart")
+  }
+  localStorage.setItem("mode5","redheart")
+});
+
+
+const heart6 = document.querySelector("#heart6");
+
+let getmode6 = localStorage.getItem("mode6");
+
+if (getmode6 === "redheart") {
+  heart6.classList.add("redheart");
+}
+heart6.addEventListener("click", () => {
+  heart6.classList.toggle("redheart");
+  if(!heart6.classList.contains("redheart")){
+    return localStorage.setItem("mode6","heart")
+  }
+  localStorage.setItem("mode6","redheart")
+});
+
+
+const heart7 = document.querySelector("#heart7");
+
+let getmode7 = localStorage.getItem("mode7");
+
+if (getmode7 === "redheart") {
+  heart7.classList.add("redheart");
+}
+heart7.addEventListener("click", () => {
+  heart7.classList.toggle("redheart");
+  if(!heart7.classList.contains("redheart")){
+    return localStorage.setItem("mode7","heart")
+  }
+  localStorage.setItem("mode7","redheart")
+});
+
+// --------------------------
